ba09abdaec00c4da9ff303b40389cc29849b0a94
Shraboni Banerjee <64948082+ShraboniBanerjee@users.noreply.github.com>
2024-02-10 12:41:02 +0530

    Add files via upload

--- Backend/Models/Users/UpdateRequest.cs
+++ Backend/Models/Users/UpdateRequest.cs
@@ -3,47 +3,27 @@ namespace WebApi.Models.Users;
 using System.ComponentModel.DataAnnotations;
 using WebApi.Entities;
 
-public class CreateRequest
+public class UpdateRequest
 {
-
-    [Required]
     public string FirstName { get; set; }
-
-    [Required]
     public string LastName { get; set; }
 
-    [Required]
     [EmailAddress]
     public string Email { get; set; }
 
 
-     [Required]
     public string CompanyName { get; set; }
-
-    [Required]
-    public string Industry { get; set; }
-
-    [Required]
+    public string Industry { get; set; } 
     public string PrimaryContact { get; set; }
-
-    [Required]
     public string AddressLine1 { get; set; }
-
     public string AddressLine2 { get; set; }
-
-    [Required]
     public string AddressState { get; set; }
-
-    [Required]
     public string AddressCity { get; set; }
-
-    [Required]
     public string AddressCountry { get; set; }
-
-    [Required]
     public string AnnualRevenue { get; set; }
-
-    [Required]
     public string Date { get; set; } 
-
+    private string replaceEmptyWithNull(string value)
+    {
+        return string.IsNullOrEmpty(value) ? null : value;
+    }
 }
